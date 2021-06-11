50430fc48f3fc1445bb116f013b3b1d2d0e589fb
hallier <ahallier@isara.fr>
2021-06-11 14:41:02 +0200

    Feat : FrontEnd add two-way binding example

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { Component1Component } from './component1/component1.component';
     Component1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
