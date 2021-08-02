bc052d9298bc80f4744ca9af093949bed0506afd
hallier <ahallier@isara.fr>
2021-08-02 14:17:57 +0200

    Feat : add transaction functionality add method to select recipient internal account email in the contacts list of the current user

--- frontend/src/app/new-transaction/new-transaction.component.ts
+++ frontend/src/app/new-transaction/new-transaction.component.ts
@@ -4,6 +4,7 @@ import {UserAccount} from "../data/userAccount";
 import {Router} from "@angular/router";
 import {AuthService} from "../services/auth.service";
 import {NgForm} from "@angular/forms";
+import {Contact} from "../data/contact";
 
 @Component({
   selector: 'app-new-transaction',
@@ -15,6 +16,9 @@ export class NewTransactionComponent implements OnInit {
   authStatus: boolean;
   transaction: Transaction;
   userAccount: UserAccount = {} as UserAccount;
+  contacts: Contact[];
+  emails: string[] = [];
+  currentSenderInternalAccountId = window.history.state;
 
   constructor(private router: Router,
               private authService: AuthService) {
@@ -23,11 +27,16 @@ export class NewTransactionComponent implements OnInit {
 
   ngOnInit(): void {
     this.authStatus = this.authService.isAuth;
+    this.authService.getContactsByUserId(this.currentSenderInternalAccountId.transferredData.data).subscribe(data => {
+      this.contacts = data;
+      for (const index in this.contacts) {
+        this.emails.push(this.contacts[index].contactEmail);
+      }
+    });
   }
 
   onSubmit(form: NgForm) {
-    const currentSenderInternalAccountId = window.history.state;
-    this.transaction.senderInternalAccountId = currentSenderInternalAccountId.transferredData.data;
+    this.transaction.senderInternalAccountId = this.currentSenderInternalAccountId.transferredData.data;
     this.authService.getUserAccountByEmail(this.transaction.recipientInternalAccountEmail).subscribe(userAccount => {
       this.userAccount = userAccount;
       this.transaction.recipientInternalAccountId = this.userAccount.internalAccountId;
